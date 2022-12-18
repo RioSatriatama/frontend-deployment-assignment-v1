b22bd291a6054d091ebb0c3c5ae817733321e03a
Fransiskus Xaverius <riosatriatama0801@gmail.com>
2022-12-18 19:52:25 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Fransiskus Xaverius Prasetyo Satriatama" // TODO: replace this
+const studentId = "FE2752149" // TODO: replace this
+
+export { url, studentName, studentId }
